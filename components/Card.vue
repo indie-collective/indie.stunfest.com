d52d87529f27f0496cd7d502b72c33d6b10cc7f5
feoche <francois.eoche@corp.ovh.com>
2019-03-28 22:47:37 +0100

    Initial commit

---
+++ components/Card.vue
@@ -0,0 +1,71 @@
+<template>
+  <div
+    class="column is-half-tablet is-one-third-desktop is-one-quarter-widescreen"
+  >
+    <div class="card is-dark">
+      <div class="card-image">
+        <figure class="image is-16by9">
+          <a :href="card.link">
+            <img v-if="card.img" :src="card.img" />
+            <img
+              v-if="!card.img"
+              src="https://place-hold.it/300x169/ffbfba/36?text=No%20images%20!&fontsize=16"
+            />
+          </a>
+        </figure>
+      </div>
+      <div class="card-content">
+        <p class="title is-3">
+          {{ card.title }}
+        </p>
+        <p class="subtitle is-5 is-family-monospace">
+          {{ card.studio }}
+        </p>
+        <p class="is-size-6">
+          <slot />
+        </p>
+      </div>
+      <footer class="card-footer">
+        <a
+          :href="card.link"
+          class="card-footer-item has-background-link has-text-white"
+          >View more...</a
+        >
+        <a
+          v-if="voteLink"
+          :href="card.voteLink"
+          class="card-footer-item has-background-primary has-text-white has-text-weight-bold is-uppercase"
+          >Vote for me !</a
+        >
+      </footer>
+    </div>
+  </div>
+</template>
+
+<style>
+.card-content {
+  min-height: 350px;
+}
+.card-content > p:last-child {
+  overflow: hidden;
+  display: -webkit-box;
+  -webkit-line-clamp: 9;
+  -webkit-box-orient: vertical;
+}
+</style>
+
+<script>
+export default {
+  props: {
+    card: {
+      type: Object,
+      required: true
+    },
+    voteLink: {
+      type: String,
+      default: '',
+      required: false
+    }
+  }
+}
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <section class="hero is-dark">
+      <div class="container has-text-centered">
+        <img src="~/assets/stunfest_logo.png" alt="Stunfest Indie Game logo" />
+      </div>
+    </section>
+    <nuxt />
+  </div>
+</template>
+<style>
+:root {
+  --primary-color: #ff5446;
+  --link-color: #3273dc;
+}
+/* Override Buefy*/
+[class*='-primary']:not([class*='background-primary']) {
+  color: var(--primary-color) !important;
+}
+.has-background-primary {
+  background: var(--primary-color) !important;
+}
+a {
+  color: var(--link-color) !important;
+}
+.has-background-link {
+  background-color: var(--link-color) !important;
+}
+
+.hero img {
+  width: 100%;
+  max-width: 30rem;
+  margin: 2rem;
+}
+</style>
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          title: 'Home',
+          icon: 'home',
+          to: { name: 'index' }
+        },
+        {
+          title: 'Inspire',
+          icon: 'lightbulb',
+          to: { name: 'inspire' }
+        }
+      ]
+    }
+  }
+}
+</script>
