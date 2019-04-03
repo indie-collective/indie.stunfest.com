615d844a71cedb81e6e7ab9a70be59671cbf4fc9
feoche <francois.eoche@corp.ovh.com>
2019-04-03 23:30:48 +0200

    UP

--- components/Card.vue
+++ components/Card.vue
@@ -4,15 +4,15 @@
   >
     <div class="card is-dark">
       <div class="card-image">
-        <figure class="image is-16by9">
-          <a :href="card.link">
-            <img v-if="card.img" :src="card.img" />
+        <a :href="card.link">
+          <figure class="image is-16by9">
+            <img v-if="card.img" :src="path(card.img)" />
             <img
               v-if="!card.img"
               src="https://place-hold.it/300x169/ffbfba/36?text=No%20images%20!&fontsize=16"
             />
-          </a>
-        </figure>
+          </figure>
+        </a>
       </div>
       <div class="card-content">
         <p class="title is-3">
@@ -32,9 +32,8 @@
           >View more...</a
         >
         <a
-          v-if="voteLink"
-          :href="card.voteLink"
-          class="card-footer-item has-background-primary has-text-white has-text-weight-bold is-uppercase"
+          href="https://xoyondo.com/ap/RAGfHVZ4lG3o80N"
+          class="card-footer-item vote-for-me has-text-weight-bold is-uppercase"
           >Vote for me !</a
         >
       </footer>
@@ -52,9 +51,66 @@
   -webkit-line-clamp: 9;
   -webkit-box-orient: vertical;
 }
+.vote-for-me {
+  --bg-color-hue: 100;
+  --bg-color-saturation: 100;
+  --bg-color-lightness: 100;
+
+  background-color: red;
+  color: white;
+  animation: color 0.5s infinite alternate;
+}
+@keyframes color {
+  0% {
+    background-color: hsl(0, 80%, 80%);
+    color: hsl(180, 100%, 60%);
+  }
+  10% {
+    background-color: hsl(20, 80%, 80%);
+    color: hsl(200, 100%, 60%);
+  }
+  20% {
+    background-color: hsl(40, 80%, 80%);
+    color: hsl(220, 100%, 60%);
+  }
+  30% {
+    background-color: hsl(60, 80%, 80%);
+    color: hsl(240, 100%, 60%);
+  }
+  40% {
+    background-color: hsl(80, 80%, 80%);
+    color: hsl(260, 100%, 60%);
+  }
+  50% {
+    background-color: hsl(100, 80%, 80%);
+    color: hsl(280, 100%, 60%);
+  }
+  60% {
+    background-color: hsl(120, 80%, 80%);
+    color: hsl(300, 100%, 60%);
+  }
+  70% {
+    background-color: hsl(140, 80%, 80%);
+    color: hsl(320, 100%, 60%);
+  }
+  80% {
+    background-color: hsl(160, 80%, 80%);
+    color: hsl(340, 100%, 60%);
+  }
+  90% {
+    background-color: hsl(180, 80%, 80%);
+    color: hsl(360, 100%, 60%);
+  }
+  100% {
+    background-color: hsl(180, 80%, 80%);
+    color: hsl(360, 100%, 60%);
+  }
+}
 </style>
 
 <script>
+/* eslint-disable no-console */
+
 export default {
   props: {
     card: {
@@ -65,6 +121,11 @@ export default {
       type: String,
       default: '',
       required: false
+    }
+  },
+  methods: {
+    path(img) {
+      return require('../assets/logos/' + img)
     }
   }
 }
