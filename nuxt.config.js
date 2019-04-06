8ab8375cb24c21c6ffce1107bd2b676ff5e66e42
feoche <francois.eoche@corp.ovh.com>
2019-04-06 17:48:40 +0200

    Font

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,7 @@ module.exports = {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Montserrat' }
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Montserrat:500,700' }
     ],
   },
 
