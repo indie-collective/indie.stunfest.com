eca629b96848774b126425efc3379a0f2a424f10
feoche <francois.eoche@corp.ovh.com>
2020-03-10 15:59:33 +0100

    UP config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,10 @@
 const years = [2013, 2014, 2015, 2016, 2018, 2019, 2020]
 
+const title = 'Indie Stunfest'
+const description = 'Welcome to the Stunfest Indie section !'
+const url = 'https://www.stunfest.com/'
+const img = 'webpage.jpg'
+
 const routerBase =
   process.env.DEPLOY_ENV === 'GH_PAGES'
     ? {
@@ -22,10 +27,27 @@ module.exports = {
    ** Headers of the page
    */
   head: {
-    title: 'Indie Stunfest',
+    title,
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'description', content: description },
+      // Open Graph / Facebook
+      { property: 'og:type', content: 'website' },
+      { property: 'og:url', content: url },
+      { property: 'og:title', content: title },
+      { property: 'og:description', content: description },
+      { property: 'og:image', content: img },
+      // Twitter
+      { property: 'twitter:card', content: 'summary_large_image' },
+      { property: 'twitter:url', content: url },
+      { property: 'twitter:title', content: title },
+      { property: 'twitter:description', content: description },
+      { property: 'twitter:image', content: img },
+      // Override
+      { hid: 'apple-mobile-web-app-title', name: 'apple-mobile-web-app-title', content: title },
+      { hid: 'author', name: 'author', content: 'Indie Collective' },
+      { hid: 'og:site_name', name: 'og:site_name', content: title }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }]
   },
