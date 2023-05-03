e79ac42a4e7b71d32856e8ab864fbd6c128d89aa
feoche <francois.eoche@corp.ovh.com>
2023-05-03 21:35:21 +0200

    Adding github pages deploy workflow

--- pages/_year.vue
+++ pages/_year.vue
@@ -7,7 +7,7 @@
           v-if="currentYear === '2023'"
           style="margin-bottom: 2em"
         >
-          À retrouver les 19 – 20 – 21 mai 2023 au Liberté à Rennes.
+          Retrouvez la liste des jeux vidéo indépendants à découvrir durant la 18ème édition du Stunfest, <br>les 19 – 20 – 21 mai 2023 au Liberté à Rennes.
         </h2>
       </div>
     </section>
