c8b6ef96a7d61bf4dc1a091f6934fba633c054f2
feoche <feoche@users.noreply.github.com>
2023-05-11 20:04:02 +0000

    deploy: b6fb7e8c8fac676f6a8dc4f8244848649d369115

--- sw.js
+++ sw.js
@@ -2,36 +2,36 @@ importScripts('/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/_nuxt/0068ba2.js",
-    "revision": "069ab87e2a870bb3973c94feb7e73547"
+    "url": "/_nuxt/0421852.js",
+    "revision": "64f7fbe3a5a6bbec36c1b3f599fda915"
   },
   {
     "url": "/_nuxt/06524eb.js",
     "revision": "70336949ca501d475925ae10277621fe"
   },
   {
-    "url": "/_nuxt/07fbf07.js",
-    "revision": "f14e0a7a620f06d7bceb6caeeb129947"
+    "url": "/_nuxt/12bc442.js",
+    "revision": "299745734385001cdb45bec1fd145488"
+  },
+  {
+    "url": "/_nuxt/5646fea.js",
+    "revision": "19234a214e67b54f13b74ebbc39891f5"
   },
   {
     "url": "/_nuxt/5a7df7c.js",
     "revision": "1d675670d640978d498e1fecbe89bb3e"
   },
   {
-    "url": "/_nuxt/61ac27c.js",
-    "revision": "f12d49cb51089a1a4ef8c50a3f35af57"
+    "url": "/_nuxt/7800065.js",
+    "revision": "826a821fd5a495d3d2538f4cd24909e6"
   },
   {
     "url": "/_nuxt/85f4284.js",
     "revision": "c00a4ff6af4f0313461502b51b03a2d8"
   },
   {
-    "url": "/_nuxt/a649f30.js",
-    "revision": "908a120ddc6f57e27bf9b98d9f1cea47"
-  },
-  {
-    "url": "/_nuxt/d6d2fc6.js",
-    "revision": "10a350c5062bc0814a3a159846997c2f"
+    "url": "/_nuxt/a751dda.js",
+    "revision": "5e7597e33c43ba8c9729e0e2cf56eb5f"
   }
 ], {
   "cacheId": "indie-nuxt",
