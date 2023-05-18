77aa56e5258a62fa74a4f371a2918852019999ce
YoruNoHikage <YoruNoHikage@users.noreply.github.com>
2023-05-18 13:21:18 +0000

    deploy: 47a092d24809223ceeafb872e3c0ae9c0343e42f

--- sw.js
+++ sw.js
@@ -6,32 +6,32 @@ workbox.precaching.precacheAndRoute([
     "revision": "70336949ca501d475925ae10277621fe"
   },
   {
+    "url": "/_nuxt/1eb2810.js",
+    "revision": "e7d597aebb0ae3276426c2428c732f9a"
+  },
+  {
+    "url": "/_nuxt/2507ca9.js",
+    "revision": "af1483d1ea98de3c703903f6673694a8"
+  },
+  {
     "url": "/_nuxt/5a7df7c.js",
     "revision": "1d675670d640978d498e1fecbe89bb3e"
   },
   {
-    "url": "/_nuxt/5f415a2.js",
-    "revision": "fa0ddfaa1a81e82161aeb1bf2295ab6c"
+    "url": "/_nuxt/5fd8bbe.js",
+    "revision": "d8a0ac4c1188e6301c011497a42d8979"
+  },
+  {
+    "url": "/_nuxt/833ec70.js",
+    "revision": "0adca6fd4ccf1135c9d815040e520c86"
   },
   {
     "url": "/_nuxt/85f4284.js",
     "revision": "c00a4ff6af4f0313461502b51b03a2d8"
   },
   {
-    "url": "/_nuxt/95fdd97.js",
-    "revision": "d23110358d5c7caefce9d583d016f100"
-  },
-  {
-    "url": "/_nuxt/a7bd1a9.js",
-    "revision": "df94745f7e1844b49c445de90fe441fb"
-  },
-  {
-    "url": "/_nuxt/c468b47.js",
-    "revision": "efdb008a5748423ba6355a0d6da6a1c5"
-  },
-  {
-    "url": "/_nuxt/d6c7f7c.js",
-    "revision": "3ef4d05568570ffe65780ebfe79fe112"
+    "url": "/_nuxt/e526625.js",
+    "revision": "0fa5efb1340f3b4341ef921b3f53901c"
   }
 ], {
   "cacheId": "indie-nuxt",
