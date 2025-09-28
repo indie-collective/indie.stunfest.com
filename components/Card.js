bb40f2905ea2728f7df9e2b863b87c3d10c380b9
Alexis Launay <yorunohikage@gmail.com>
2025-09-28 18:37:36 +0200

    Fix crash when no link is provided

--- components/Card.js
+++ components/Card.js
@@ -2,23 +2,25 @@ export default {
   props: {
     card: {
       type: Object,
-      required: true
+      required: true,
     },
     currentYear: {
       type: String,
-      default: () => location.href.replace(/.*?\/(\d{4})(\/.*?)?$/gim, '$1')
-    }
+      default: () => location.href.replace(/.*?\/(\d{4})(\/.*?)?$/gim, '$1'),
+    },
   },
   computed: {
     fileName() {
       let img = ''
       try {
-        img = require(`~/static/${
-          this.currentYear
-        }/${this.card.title
+        img = require(`~/static/${this.currentYear}/${this.card.title
           .toLowerCase()
           .replace(/[^a-zA-Z0-9]|-|\s/gim, '')}.jpg`)
       } catch (e) {
+        console.log(
+          'could not find',
+          this.card.title.toLowerCase().replace(/[^a-zA-Z0-9]|-|\s/gim, '')
+        )
         try {
           img = require(`~/static/${this.currentYear}/_tba.jpg`)
         } catch (e) {
@@ -28,26 +30,28 @@ export default {
       return img
     },
     mainLink() {
-      let { link } = this.card;
+      let { link } = this.card
 
-      const url = new URL(link);
-      if (url.hostname === 'store.steampowered.com') {
-        // let's give us some views :P
-        url.searchParams.set('curator_clanid', '42527171');
+      try {
+        const url = new URL(link)
+        if (url.hostname === 'store.steampowered.com') {
+          // let's give us some views :P
+          url.searchParams.set('curator_clanid', '42527171')
 
-        link = url.toString();
-      }
+          link = url.toString()
+        }
+      } catch (err) {}
 
-      return link;
-    }
+      return link
+    },
   },
   methods: {
     getNetworkImg(network) {
       return require(`~/static/${network}.png`)
-    }
+    },
   },
   mounted() {
     const currentYear = this.$route.params.year
     return { currentYear }
-  }
+  },
 }
