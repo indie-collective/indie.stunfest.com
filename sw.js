0f32d22b1e62daccdfb77f1070ad8a159e422fc2
YoruNoHikage <YoruNoHikage@users.noreply.github.com>
2025-10-06 19:38:21 +0000

    deploy: 660d1ca4deea1c6fb593e9a9e416527c8329cba7

--- sw.js
+++ sw.js
@@ -10,12 +10,12 @@ workbox.precaching.precacheAndRoute([
     "revision": "d7fd2320002ffaba0299cda1c7279e28"
   },
   {
-    "url": "/_nuxt/5dc089b.js",
-    "revision": "b0485689e32f5c30332ec7fb8589e9e9"
+    "url": "/_nuxt/45980fb.js",
+    "revision": "629e7df0358607b8f76c2d099e4c64b0"
   },
   {
-    "url": "/_nuxt/674a9d4.js",
-    "revision": "b3743a9e265d85f227df7acba155c551"
+    "url": "/_nuxt/5dc089b.js",
+    "revision": "b0485689e32f5c30332ec7fb8589e9e9"
   },
   {
     "url": "/_nuxt/85f4284.js",
@@ -26,8 +26,8 @@ workbox.precaching.precacheAndRoute([
     "revision": "b272dcbc690cfb4334bf528c64258236"
   },
   {
-    "url": "/_nuxt/b1da2f7.js",
-    "revision": "af9271d00fd4bd1b3ace06d216831bac"
+    "url": "/_nuxt/c0ccad8.js",
+    "revision": "f6d547241dd4baf6e205705a62a194ca"
   },
   {
     "url": "/_nuxt/fef5467.js",
